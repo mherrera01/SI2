38a060c9894dea44127ccdd180440ee25c7f0b55
mherrera01 <miguelherrera8600@gmail.com>
2021-03-19 19:51:02 +0100

    Exercise 12 done

--- P1B/P1-jms/src/clientjms/ssii2/VisaQueueMessageProducer.java
+++ P1B/P1-jms/src/clientjms/ssii2/VisaQueueMessageProducer.java
@@ -17,7 +17,10 @@ public class VisaQueueMessageProducer {
     // Anotamos los siguientes objetos para
     // conectar con la connection factory y con la cola
     // definidas en el enunciado
+    @Resource(mappedName = "jms/VisaConnectionFactory")
     private static ConnectionFactory connectionFactory;
+
+    @Resource(mappedName = "jms/VisaPagosQueue")
     private static Queue queue;
 
     // Método de prueba
@@ -65,8 +68,14 @@ public class VisaQueueMessageProducer {
         }
 
         try {
-          // TODO: Inicializar connectionFactory
+          // Inicializamos connectionFactory
           // y queue mediante JNDI
+
+          /*
+          InitialContext jndi = new InitialContext();
+          connectionFactory = (ConnectionFactory)jndi.lookup("jms/VisaConnectionFactory");
+          queue = (Queue)jndi.lookup("jms/VisaPagosQueue");
+          */
 
           connection = connectionFactory.createConnection();
           session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
