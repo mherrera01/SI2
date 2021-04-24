dbdf85510fb804f8649f12af0861cf20177e402d
mherrera01 <miguelherrera8600@gmail.com>
2021-04-24 21:09:04 +0200

    Exercise 5 done

--- P2/ejecutarProyectos.sh
+++ P2/ejecutarProyectos.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-for i in P1-base P1-ws P1-ejb-servidor-remoto P1-ejb-cliente-remoto; do
+for i in P1-base P1-ejb-servidor-remoto P1-ejb-cliente-remoto; do
 	cd $i
 	ant replegar; ant delete-pool-local
 	cd -
 done
 
-cd P1-base
-ant delete-db
+cd P1-ws
+ant replegar-servicio; ant replegar-cliente
+cd -
 
 for i in P1-base P1-ws P1-ejb-servidor-remoto P1-ejb-cliente-remoto; do
 	cd $i
